597cf6d9cde9eeab77b5389708ccddecf5e7aa0f
Alexandr Bur <buralex89@gmail.com>
2017-07-14 00:21:31 +0300

    not

--- backend/web/js/drivers.js
+++ backend/web/js/drivers.js
@@ -42,44 +42,59 @@ document.querySelector("#coordinates").addEventListener("submit", function (e) {
 
 
 function initMap(drivers) {
+    
+    var driversRoad;
 
     var bounds = new google.maps.LatLngBounds;
     var markersArray = [];
     
     console.log( drivers );
+    
+    var destinations = [];
+    
+for ( var n = 0; n < drivers[1].length; n++ ) {
+    
+    var coord = {
+        lat: parseFloat(drivers[1][n].lat),
+        lng: parseFloat(drivers[1][n].lng)
+    }
+    console.log( coord );
+    destinations.push(coord);
+    
+}
+
+    //console.log( destinations );
 
 
-
-
-    var destinations = [
-        {lat: 47.911529, lng: -121.606417},
-        {lat: 48.072560, lng: -121.689606},
-        {lat: 48.092397, lng: -122.576537},
-        {lat: 48.950253, lng: -122.459792},
-    ];
+//    var destinations = [
+//        {lat: 47.911529, lng: -121.606417},
+//        {lat: 48.072560, lng: -121.689606},
+//        {lat: 48.092397, lng: -122.576537},
+//        {lat: 48.950253, lng: -122.459792},
+//    ];
 
     var origin1 = {lat: 37.99616268, lng: -91.93359375};
     //var origin2 = 'Greenwich, England';
     //var destinationA = 'Stockholm, Sweden';
     var destinationB = {lat: 53.61857936, lng: -113.53271484};
 
-//    var destinationIcon = 'https://chart.googleapis.com/chart?' +
-//            'chst=d_map_pin_letter&chld=D|FF0000|000000';
-//    var originIcon = 'https://chart.googleapis.com/chart?' +
-//            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
-//    var map = new google.maps.Map(document.getElementById('map'), {
-//        center: {lat: 55.53, lng: 9.4},
-//        zoom: 10
-//    });
+    var destinationIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=D|FF0000|000000';
+    var originIcon = 'https://chart.googleapis.com/chart?' +
+            'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 55.53, lng: 9.4},
+        zoom: 10
+    });
     var geocoder = new google.maps.Geocoder;
 
     var service = new google.maps.DistanceMatrixService;
     
-    for ( var x = 0; x < drivers[1].length; x++ ) {
+//    for ( var x = 0; x < drivers[1].length; x++ ) {
         
             service.getDistanceMatrix({
         origins: [{ lat: parseFloat(drivers[0].orig_lat), lng: parseFloat(drivers[0].orig_lng) }],
-        destinations: [{ lat: parseFloat(drivers[1][x].lat), lng: parseFloat(drivers[1][x].lng) }],
+        destinations: destinations,
         travelMode: 'DRIVING',
         unitSystem: google.maps.UnitSystem.METRIC,
         avoidHighways: false,
@@ -92,23 +107,25 @@ function initMap(drivers) {
             var destinationList = response.destinationAddresses;
             var outputDiv = document.getElementById('output');
             outputDiv.innerHTML = '';
-//            deleteMarkers(markersArray);
+            deleteMarkers(markersArray);
 
-//            var showGeocodedAddressOnMap = function (asDestination) {
-//                var icon = asDestination ? destinationIcon : originIcon;
-//                return function (results, status) {
-//                    if (status === 'OK') {
-//                        map.fitBounds(bounds.extend(results[0].geometry.location));
-//                        markersArray.push(new google.maps.Marker({
-//                            map: map,
-//                            position: results[0].geometry.location,
-//                            icon: icon
-//                        }));
-//                    } else {
-//                        alert('Geocode was not successful due to: ' + status);
-//                    }
-//                };
-//            };
+            var showGeocodedAddressOnMap = function (asDestination) {
+                var icon = asDestination ? destinationIcon : originIcon;
+                return function (results, status) {
+                    if (status === 'OK') {
+                        map.fitBounds(bounds.extend(results[0].geometry.location));
+                        markersArray.push(new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location,
+                            icon: icon,
+                            title: results[0].formatted_address
+                        }));
+                        //console.log( results[0].formatted_address );
+                    } else {
+                        alert('Geocode was not successful due to: ' + status);
+                    }
+                };
+            };
             //console.log(google.maps.UnitSystem);
             //console.log(originList);
 
@@ -121,17 +138,21 @@ function initMap(drivers) {
 
 
 
-//                geocoder.geocode({'address': originList[i]},
-//                        showGeocodedAddressOnMap(false));
+                geocoder.geocode({'address': originList[i]},
+                        showGeocodedAddressOnMap(false));
 
                 for (var j = 0; j < results.length; j++) {
 
                     //console.log(destinationList[j]);
 
-//                    geocoder.geocode({'address': destinationList[j]},
-//                            showGeocodedAddressOnMap(true));
+                    geocoder.geocode({'address': destinationList[j]},
+                            showGeocodedAddressOnMap(true));
 
                     //console.log(results[j].duration);
+                    
+                    drivers[1][j].distanceRoad = results[j].distance.text;
+                    
+                    
 
                     outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
                             ': ' + results[j].distance.text + ' in ' +
@@ -141,17 +162,23 @@ function initMap(drivers) {
         }
     });
         
-    }
+//    }
+
+    driversRoad = drivers;
     
+    console.log( 'drivers end' );
     
+    console.log( driversRoad );
 
 }
 
-//function deleteMarkers(markersArray) {
-//    for (var i = 0; i < markersArray.length; i++) {
-//        markersArray[i].setMap(null);
-//    }
-//    markersArray = [];
-//}
+
+
+function deleteMarkers(markersArray) {
+    for (var i = 0; i < markersArray.length; i++) {
+        markersArray[i].setMap(null);
+    }
+    markersArray = [];
+}
 
 
